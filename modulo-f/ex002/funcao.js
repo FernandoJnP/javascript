cfd7f8bd806de397506b85c46579a06ec0eefcc1
Fernando Pessoa <fernandojosedonascimentop11@gmail.com>
2023-05-12 15:52:42 -0300

    funçao

---
+++ modulo-f/ex002/funcao.js
@@ -0,0 +1,12 @@
+function parimpar(n){
+    if(n%2 == 0){
+        return'Par'
+    }else{
+        return'impar'
+    }
+}
+console.log(parimpar(3))
+
+/*Metodos diferentes
+let res = parimpar(3)
+console.log(res)*/
